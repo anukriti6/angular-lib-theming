79d9800afdb58d9554b33a81a5a444eae12ae62d
anukriti6 <anukritigupta1988@gmail.com>
2020-12-02 23:27:27 +0000

    added theme in lib and use in application

--- NXWS/mybank/apps/appone/src/app/app.module.ts
+++ NXWS/mybank/apps/appone/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UiModule } from '@mybank/ui';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule,UiModule,MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
