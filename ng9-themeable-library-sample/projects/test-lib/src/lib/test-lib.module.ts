f85138e7ad777585db8c7d656b64889e919d9ffb
anukriti6 <anukritigupta1988@gmail.com>
2020-12-02 20:02:36 +0000

    added theme to app and create component in lib.

--- ng9-themeable-library-sample/projects/test-lib/src/lib/test-lib.module.ts
+++ ng9-themeable-library-sample/projects/test-lib/src/lib/test-lib.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { TestLibComponent } from './test-lib.component';
+import { MyButtonComponent } from './my-button/my-button.component';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
 @NgModule({
-  declarations: [TestLibComponent],
+  declarations: [TestLibComponent, MyButtonComponent],
   imports: [
+    MatButtonModule
   ],
-  exports: [TestLibComponent]
+  exports: [TestLibComponent,MyButtonComponent]
 })
 export class TestLibModule { }
--- ng9-themeable-library-sample/projects/web-app/src/app/app.module.ts
+++ ng9-themeable-library-sample/projects/web-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { TestLibModule } from 'projects/test-lib/src/public-api';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    TestLibModule,
     MatButtonModule
   ],
   providers: [],
